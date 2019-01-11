410795af4d1edfe51ef246fd12b7c6143b583d7c
jnarinoc <jnarinoc@ucentral.edu.co>
2019-01-11 09:20:01 -0500

    CREACION DE AUTENTICACION BASICA CON AUTH0

---
+++ authapp/src/app/services/auth-guard.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from '@angular/router';
+import { AuthService } from './auth.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor(private auth: AuthService) { }
+
+  canActivate( next: ActivatedRouteSnapshot, state: RouterStateSnapshot ) {
+    console.log(next);
+    if (this.auth.isAuthenticated()) {
+      console.log('Paso por el Guard');
+      return true;
+    } else {
+      console.error('Bloqueado por el Guard');
+      return false;
+    }
+  }
+}
